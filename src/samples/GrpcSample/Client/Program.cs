d7d1191fc9f4c015043998f0f585a0008d15a658
Marc Delorme <delormemarco@gmail.com>
2021-02-02 20:16:22 +0900

    Add Grpc Sample

---
+++ src/samples/GrpcSample/Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using GrpcSample.Generated;
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Grpc.Core.Channel channel = new Grpc.Core.Channel("127.0.0.1:30051", Grpc.Core.ChannelCredentials.Insecure);
+
+            var client = new Calculator.CalculatorClient(channel);
+
+            var request = new IntegerMessage { Value = 56, Message = "Hello Grpc World" };
+            Console.WriteLine($"Request: Value = {request.Value}, Message = {request.Message}");
+            var reply = client.AddOne(request);
+            Console.WriteLine($"Reply: Value = {reply.Value}, Message = {reply.Message}");
+
+            await channel.ShutdownAsync();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/samples/GrpcSample/Server/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using GrpcSample.Generated;
+
+namespace GrpcSample
+{
+    class CalculatorImpl : Calculator.CalculatorBase
+    {
+        // Server side handler of the SayHello RPC
+        public override Task<IntegerMessage> AddOne(IntegerMessage request, Grpc.Core.ServerCallContext context)
+        {
+            return Task.Run(() =>
+            {
+                Console.WriteLine($"Receive Request: Value = {request.Value}, Message = {request.Message}");
+                return new IntegerMessage { Value = request.Value + 1, Message = request.Message };
+            });
+        }
+    }
+
+    class Program
+    {
+        const int Port = 30051;
+
+        static async Task Main(string[] args)
+        {
+            Grpc.Core.Server server = new Grpc.Core.Server
+            {
+                Services = { Calculator.BindService(new CalculatorImpl()) },
+                Ports = { new Grpc.Core.ServerPort("localhost", Port, Grpc.Core.ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine("Calculator server listening on port " + Port);
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+
+            await server.ShutdownAsync();
+        }
+    }
+}
